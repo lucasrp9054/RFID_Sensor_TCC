167ab3ecfce9b737d8a30c6d65899ae661312161
Lucas.R <lucasrp9054@users.noreply.github.com>
2024-06-16 20:38:41 -0300

    Update functions.php

--- php/functions.php
+++ php/functions.php
@@ -17,7 +17,7 @@ function checkAndHandleRegistration($uid_rfid, $pdo) {
 
         if ($stmt_profissional->fetchColumn()) {
             echo "UID corresponde a um profissional.\n";
-            handleProfissionalRegistration($uid_rfid, $pdo);
+            entrada_saida_Profissionais($uid_rfid, $pdo);
             return; // Retorna após tratamento como profissional
         }
 
@@ -29,7 +29,7 @@ function checkAndHandleRegistration($uid_rfid, $pdo) {
 
         if ($stmt_aluno->fetchColumn()) {
             echo "UID corresponde a um aluno.\n";
-            handleAlunoRegistration($uid_rfid, $pdo);
+            entrada_saida_Alunos($uid_rfid, $pdo);
             return; // Retorna após tratamento como aluno
         }
 
@@ -42,13 +42,8 @@ function checkAndHandleRegistration($uid_rfid, $pdo) {
 
 
 
-
-
-
-
-
 // Função para lidar com registros de profissionais na tabela de presença
-function handleProfissionalRegistration($uid_rfid, $pdo) {
+function entrada_saida_Profissionais($uid_rfid, $pdo) {
     echo "Processando registro de profissional para UID: $uid_rfid\n";
     // Verifica se já existe um registro para o ID RFID na tabela de registros de profissionais
     $stmt_registro = $pdo->prepare("SELECT * FROM tb_registro_profissionais WHERE uid_rfid = ? ORDER BY data_hora_entrada DESC LIMIT 1");
@@ -60,18 +55,18 @@ function handleProfissionalRegistration($uid_rfid, $pdo) {
         $sql_update = "UPDATE tb_registro_profissionais SET data_hora_saida = NOW() WHERE uid_rfid = ? AND id_registro_profissional = ?";
         $stmt_update = $pdo->prepare($sql_update);
         $stmt_update->execute([$uid_rfid, $registro['id_registro_profissional']]);
-        echo "Horário de saída atualizado para o registro de profissional.\n";
+        echo "Horário de saída atualizado para o registro de presença do profissional.\n";
     } else {
         // Cria um novo registro de entrada
         $sql_insert = "INSERT INTO tb_registro_profissionais (uid_rfid, data_hora_entrada) VALUES (?, NOW())";
         $stmt_insert = $pdo->prepare($sql_insert);
         $stmt_insert->execute([$uid_rfid]);
-        echo "Novo registro de entrada criado para profissional.\n";
+        echo "Novo registro de presença criado para profissional.\n";
     }
 }
 
 // Função para lidar com registros de alunos na tabela de presença
-function handleAlunoRegistration($uid_rfid, $pdo) {
+function entrada_saida_Alunos($uid_rfid, $pdo) {
     echo "Processando registro de aluno para UID: $uid_rfid\n";
     // Verifica se já existe um registro para o ID RFID na tabela de registros de alunos
     $stmt_registro = $pdo->prepare("SELECT * FROM tb_registro_alunos WHERE uid_rfid = ? ORDER BY data_hora_entrada DESC LIMIT 1");
@@ -83,31 +78,118 @@ function handleAlunoRegistration($uid_rfid, $pdo) {
         $sql_update = "UPDATE tb_registro_alunos SET data_hora_saida = NOW() WHERE uid_rfid = ? AND id_registro_aluno = ?";
         $stmt_update = $pdo->prepare($sql_update);
         $stmt_update->execute([$uid_rfid, $registro['id_registro_aluno']]);
-        echo "Horário de saída atualizado para o registro de aluno.\n";
+        echo "Horário de saída atualizado para o registro de presença do aluno.\n";
     } else {
         // Cria um novo registro de entrada
         $sql_insert = "INSERT INTO tb_registro_alunos (uid_rfid, data_hora_entrada) VALUES (?, NOW())";
         $stmt_insert = $pdo->prepare($sql_insert);
         $stmt_insert->execute([$uid_rfid]);
-        echo "Novo registro de entrada criado para aluno.\n";
+        echo "Novo registro de presença criado para aluno.\n";
     }
 }
 
-//Verificar se o RA bate com o banco de dados
-function validateRA($ra, $pdo){
+function validarLogin($ma, $senha, $pdo) {
+    $senha_md5 = md5($senha); // Transformando a senha inserida em md5
 
+    $sql_check = "SELECT senha FROM tb_profissionais WHERE ma = :ma";
+    $stmt_check = $pdo->prepare($sql_check);
+    $stmt_check->bindParam(':ma', $ma);
+    $stmt_check->execute();
+    $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
 
+    if ($result) { // Profissional encontrado
 
-    firstAccess($ra, $pdo);
+        if ($result['senha'] === $senha_md5)
+        { // Senha corresponde
+            // Realizar login
+        }
+        else
+        { // Senha inválida
+            // Enviar mensagem de erro
+        }
+    }
+    else
+    { // Profissional não encontrado, procurar na tb_alunos
+        $sql_check = "SELECT senha FROM tb_alunos WHERE ma = :ma";
+        $stmt_check = $pdo->prepare($sql_check);
+        $stmt_check->bindParam(':ma', $ma);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
 
+        if ($result && $result['senha'] === $senha_md5){ // Senha corresponde
+            // Realizar login
+        } else { // Senha inválida
+            // Enviar mensagem de erro
+        }
+    }
 }
 
-//Realiza o cadastro das informações faltantes no banco de dados
-function firstAccess($uid_rfid, $pdo){
 
 
+//Primeiro acesso ao sistema web
+function existeDadosVazios($ma, $pdo) {
+    $sql_check = "SELECT senha FROM tb_profissionais WHERE ma = :ma";
+    $stmt_check = $pdo->prepare($sql_check);
+    $stmt_check->bindParam(':ma', $ma);
+    $stmt_check->execute();
+    $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+    if ($result) { // Profissional encontrado
+        if (empty($result['senha'])) { // Senha vazia
+            // Redirecionar para cadastro de nova senha
+        }
+    } else { // Profissional não encontrado, procurar na tb_alunos
+        $sql_check = "SELECT senha FROM tb_alunos WHERE ma = :ma";
+        $stmt_check = $pdo->prepare($sql_check);
+        $stmt_check->bindParam(':ma', $ma);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) { // Aluno encontrado
+            
+            if (empty($result['senha'])) { // Senha vazia
+                // Redirecionar para forms de cadastro
+            }
+        }
+        else
+        {
+            return 0; // Não é o primeiro acesso desse MA, tentar logar
+        }
+    }
+}
 
 
+function primeiroAcessoProfissional($ma, $senha, $pdo) {
+
+    $senha_md5 = md5($senha); // Transformando a senha em md5
+
+    $sql = "UPDATE tb_profissionais SET senha = :senha WHERE ma = :ma";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':senha', $senha_md5);
+    $stmt->bindParam(':ma', $ma);
+    $stmt->execute();
+}
 
 
+//Realiza o cadastro das informações faltantes no banco de dados dos alunos
+function primeiroAcessoAluno($ma, $nome, $data_nasc, $cpf, $email, $telefone, $senha, $pdo) {
+    $senha_md5 = md5($senha); // Transformando a senha em md5
+
+    $sql = "UPDATE tb_alunos 
+            SET nome = :nome, data_nascimento = :data_nasc, cpf = :cpf, email = :email, telefone = :telefone, senha = :senha 
+            WHERE ma = :ma";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':name', $nome);
+    $stmt->bindParam(':data_nasc', $data_nasc);
+    $stmt->bindParam(':cpf', $cpf);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':telefone', $telefone);
+    $stmt->bindParam(':senha', $senha_md5);
+    $stmt->bindParam(':ma', $ma);
+    $stmt->execute();
+
+    // Redirecionar para página de login
+
+    return $pdo->lastInsertId();
 }
